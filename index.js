b6823803dd1938ddb93a8b9217be940c7320ccf2
Prince Ndebele <princendex460@hotmail.com>
2021-11-07 21:53:16 +0100

    We created functionality for employees, we tested it and it ran perfect

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const inquirer = requirer("inquirer");
+const inquirer = require("inquirer");
 const Manager = require("./library/Manager");
 const Intern = require("./library/Intern");
 const Engineer = require("./library/Engineer");
+let employeesDB =""
 
-
+startTeamInputs()
 function startTeamInputs(){
     inquirer.prompt([
         {
-            type:"input",
+            type:"list",
             name:"entry",
             message: "What would you like to do?",
             choices:["Add Engineer","Add Manager","Add Intern","Exit App"]
@@ -30,4 +31,39 @@ function startTeamInputs(){
 
         }
     })
+}
+
+
+function hireManager(){
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter Manager name",
+            name:"ename"
+        },
+        {
+            type:"input",
+            message:"Enter Manager ID",
+            name:"id"
+        },
+        {
+            type:"input",
+            message:"Enter Manager email",
+            name:"email"
+        },
+        {
+            type:"input",
+            message:"Enter Manager Officenumber",
+            name:"key"
+        }
+    ]).then(({ename,id,email,key})=> {
+       const newManager = new Manager(ename,id,email,key);
+       employeesDB += ``
+       startTeamInputs()
+    })
+}
+
+
+function exitApp(){
+    process.exit(0);
 }
