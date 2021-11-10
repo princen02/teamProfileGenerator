4b07857e9a48b53121b4f5c67394a6bf21570abe
Prince Ndebele <princendex460@hotmail.com>
2021-11-10 21:28:10 +0100

    We were able to make each emplyee run on the website

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const Manager = require("./library/Manager");
 const Intern = require("./library/Intern");
 const Engineer = require("./library/Engineer");
+const fs = require("fs")
 let employeesDB =""
 
 startTeamInputs()
@@ -16,7 +17,7 @@ function startTeamInputs(){
     ]).then(response =>{
         switch(response.entry){
             case "Add Engineer":
-                 hireEnginner();
+                 hireEngineer();
                  break;
             case "Add Manager":
                 hireManager();
@@ -58,12 +59,140 @@ function hireManager(){
         }
     ]).then(({ename,id,email,key})=> {
        const newManager = new Manager(ename,id,email,key);
-       employeesDB += ``
+       employeesDB += `
+       <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Manager</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${newManager.name}</h6>
+    <p class="card-text">${newManager.id}</p>
+    <p class="card-text">${newManager.officeNumeber}</p>
+    <a href="mail:${newManager.email}" class="card-link">${newManager.email}</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+       `
+       startTeamInputs()
+    })
+}
+function hireEngineer(){
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter Engineer name",
+            name:"ename"
+        },
+        {
+            type:"input",
+            message:"Enter Engineer ID",
+            name:"id"
+        },
+        {
+            type:"input",
+            message:"Enter Engineer email",
+            name:"email"
+        },
+        {
+            type:"input",
+            message:"Enter Engineer Officenumber",
+            name:"key"
+        }
+    ]).then(({ename,id,email,key})=> {
+       const newEngineer = new Engineer(ename,id,email,key);
+       employeesDB += `
+       <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Engineer</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${newEngineer.name}</h6>
+    <p class="card-text">${newEngineer.id}</p>
+    <p class="card-text">${newEngineer.officeNumeber}</p>
+    <a href="mail:${newEngineer.email}" class="card-link">${newEngineer.email}</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+       `
+       startTeamInputs()
+    })
+}
+function hireIntern(){
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter Intern name",
+            name:"ename"
+        },
+        {
+            type:"input",
+            message:"Enter Intern ID",
+            name:"id"
+        },
+        {
+            type:"input",
+            message:"Enter Intern email",
+            name:"email"
+        },
+        {
+            type:"input",
+            message:"Enter Intern Officenumber",
+            name:"key"
+        }
+    ]).then(({ename,id,email,key})=> {
+       const newIntern = new Intern(ename,id,email,key);
+       employeesDB += `
+       <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Intern</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${newIntern.name}</h6>
+    <p class="card-text">${newIntern.id}</p>
+    <p class="card-text">${newIntern.officeNumeber}</p>
+    <a href="mail:${newIntern.email}" class="card-link">${newIntern.email}</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+       `
        startTeamInputs()
     })
 }
 
-
 function exitApp(){
+    var StarterHTML=`
+    <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>`
+
+    var ClosingHTML =`
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+    `
+    var HtmlFile = StarterHTML + employeesDB + ClosingHTML
+    console.log(HtmlFile)
+    fs.writeFileSync("./index.html",HtmlFile,function(err,dat){
+        if (err) throw err;
+    })
+
+    console.log("------------------------Team File created----------------------------")
+
+
     process.exit(0);
-}
+}                                                                         
--- library/Engineer.js
+++ library/Engineer.js
@@ -2,7 +2,7 @@ const Employee = require("./Employee");
 class Engineer extends Employee{
     constructor(name,id,email,github){
        super(name,id,email)
-       this.github = githbub
+       this.github = github
     }
     getGithub(){
         return this.github;
